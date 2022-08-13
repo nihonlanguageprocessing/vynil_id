cbec90f74d48db4930e45a4a337f10e97785b527
Nathan Port <nathan.port@yahoo.com>
2022-08-13 17:54:44 +0900

    geometric tools

--- vynil_id/utils/geo_tools.py
+++ vynil_id/utils/geo_tools.py
@@ -1,61 +1,51 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from utils import seg_intersect
+from vynil_id.utils import vector_tools
 
-def min_quad_oct(contours):
+def max_directional_oct(contour):
     '''From a contours, finds maxiumum points in each of 8 directions'''
-    rs_hull = [np.squeeze(h,axis=1) for h in contours]
-    oct_points = []
-    for h in rs_hull:
-        x,y=np.split(h,2,axis=1)
+    rs_contour = np.squeeze(contour,axis=1)
 
-        ## gets the maximum point in 8 cardinal directions
-        ## 4 cardinal directions
-        max_e = int(max(x))
-        max_w = int(min(x))
-        max_n = int(max(y))
-        max_s = int(min(y))
+    x,y=np.split(rs_contour,2,axis=1)
+    ## gets the maximum point in 8 cardinal directions
+    ## 4 cardinal directions
+    max_e = int(max(x))
+    max_w = int(min(x))
+    max_n = int(max(y))
+    max_s = int(min(y))
+    max_e_ = [max_e, int(min(y[np.where(x == max_e)[0]]))]
+    max_s_ = [int(min(x[np.where(y == max_s)[0]])), max_s]
+    max_w_ = [max_w, int(min(y[np.where(x == max_w)[0]]))]
+    max_n_ = [int(min(x[np.where(y == max_n)[0]])), max_n]
+    ## diagonal cardinal directions
+    nw = -x+y
+    ne = x+y
+    se = x-y
+    sw = -x-y
+    max_nw = int(max(nw))
+    max_ne = int(max(ne))
+    max_se = int(max(se))
+    max_sw = int(max(sw))
+    max_nw_i = np.argmax(nw == max_nw)
+    max_ne_i = np.argmax(ne == max_ne)
+    max_se_i = np.argmax(se == max_se)
+    max_sw_i = np.argmax(sw == max_sw)
+    max_nw_ = [int(x[max_nw_i]), int(y[max_nw_i])]
+    max_ne_ = [int(x[max_ne_i]), int(y[max_ne_i])]
+    max_se_ = [int(x[max_se_i]), int(y[max_se_i])]
+    max_sw_ = [int(x[max_sw_i]), int(y[max_sw_i])]
+    ## creates an onctagon that binds the hull
+    oct_point = [max_w_, max_nw_, max_n_, max_ne_, max_e_, max_se_, max_s_, max_sw_]
+    ## convert to contour
+    oct_point = np.array(oct_point).reshape((-1,1,2)).astype(np.int32)
 
-        max_e_ = [max_e, int(min(y[np.where(x == max_e)[0]]))]
-        max_s_ = [int(min(x[np.where(y == max_s)[0]])), max_s]
-        max_w_ = [max_w, int(min(y[np.where(x == max_w)[0]]))]
-        max_n_ = [int(min(x[np.where(y == max_n)[0]])), max_n]
-
-        ## diagonal cardinal directions
-        nw = -x+y
-        ne = x+y
-        se = x-y
-        sw = -x-y
-
-        max_nw = int(max(nw))
-        max_ne = int(max(ne))
-        max_se = int(max(se))
-        max_sw = int(max(sw))
-
-        max_nw_i = np.argmax(nw == max_nw)
-        max_ne_i = np.argmax(ne == max_ne)
-        max_se_i = np.argmax(se == max_se)
-        max_sw_i = np.argmax(sw == max_sw)
-
-        max_nw_ = [int(x[max_nw_i]), int(y[max_nw_i])]
-        max_ne_ = [int(x[max_ne_i]), int(y[max_ne_i])]
-        max_se_ = [int(x[max_se_i]), int(y[max_se_i])]
-        max_sw_ = [int(x[max_sw_i]), int(y[max_sw_i])]
-
-        ## creates an onctagon that binds the hull
-        oct_point = [max_w_, max_nw_, max_n_, max_ne_, max_e_, max_se_, max_s_, max_sw_]
-
-        ## convert to contour
-        oct_point = np.array(oct_point).reshape((-1,1,2)).astype(np.int32)
-        oct_points.append(oct_point)
-
-    return oct_points
+    return oct_point
 
 def point_distances(points):
     '''get distances between sequential points
     Points should be ordered either counter or clockwise'''
     dists = []
-    for i in range(-1,7):
+    for i in range(-1,len(points)-1):
         dists.append(np.linalg.norm(points[i]-points[i+1]))
     return dists
 
@@ -67,7 +57,7 @@ def longest_line_indices(dists, num_lines=4):
     ## get pairs of points forming longest lines
     indices_pairs = []
     for i in ind:
-        indices_pairs.append([(i-1) % 8, i])
+        indices_pairs.append([(i-1) % (len(dists)), i])
 
     return indices_pairs
 
@@ -81,7 +71,7 @@ def longest_line_intersections(points, indices_pairs):
         p3 = points[indices_pairs[i+1][0]][0]
         p4 = points[indices_pairs[i+1][1]][0]
 
-        ngon_points.append(seg_intersect(p1,p2,p3,p4))
+        ngon_points.append(vector_tools.seg_intersect(p1,p2,p3,p4))
 
     ## Convert to contour
     ngon = np.stack(ngon_points, axis=0).reshape((-1,1,2)).astype(np.int32)
