4841ee82e4ce4fbb1f70026f88d8e11bd5d36b3e
Kritsada Saengtub <kritsadasaengtub@gmail.com>
2022-08-26 17:14:32 +0900

    2022-08-26 updated

--- vynil_id/download_google_images.py
+++ vynil_id/download_google_images.py
@@ -23,7 +23,7 @@ from google_images_download import google_images_download
 #instantiate the class
 response = google_images_download.googleimagesdownload()
 arguments = {"keywords":"Anri (2) - Bi・Ki・Ni, Anri (2) - Coool, Anri (2) - Timely!!, Anri (2) - Wave, Hiroshi Sato - Aqua, Hiroshi Sato - Awakening, Hiroshi Sato - Future File, Hiroshi Sato - Sound Of Science, Mariya Takeuchi - Beginning, Mariya Takeuchi - Love Songs, Mariya Takeuchi - Miss M, Mariya Takeuchi - Request, Mariya Takeuchi - Trad = トラッド, Mariya Takeuchi - University Street, Momoko Kikuchi - Adventure, Momoko Kikuchi - Escape From Dimension, Momoko Kikuchi - Ocean Side, Momoko Kikuchi - Tropic Of Capricorn =トロピック・オブ・カプリコーン 南回帰線, Taeko Ohnuki - Aventure, Taeko Ohnuki - Cliche, Taeko Ohnuki - Grey Skies, Taeko Ohnuki - Mignonne, Taeko Ohnuki - Romantique, Taeko Ohnuki - Sunshower, Tatsuro Yamashita - Big Wave = ビッグウェイブ, Tatsuro Yamashita - Circus Town, Tatsuro Yamashita - For You, Tatsuro Yamashita - Go Ahead!, Tatsuro Yamashita - It's A Poppin' Time, Tatsuro Yamashita - Melodies, Tatsuro Yamashita - Moonglow, Tatsuro Yamashita - On The Street Corner, Tatsuro Yamashita - On The Street Corner 2, Tatsuro Yamashita - Ride On Time, Tatsuro Yamashita - Softly, Tatsuro Yamashita - Spacy, Utada Hikaru - Badモード, Utada Hikaru - Deep River, Utada Hikaru - First Love, Various - Classic Jazz-Funk Mastercuts Volume 7, 東北新幹線 - Thru Traffic",
-             "limit":7,"print_urls":False
+             "limit":3,"print_urls":False
              }
 
 paths = response.download(arguments)
