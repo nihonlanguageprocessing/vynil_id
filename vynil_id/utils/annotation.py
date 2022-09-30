f6fbb211f294ae1c1c3519b939b24b1436f9082a
Nathan Port <nathan.port@yahoo.com>
2022-09-30 19:27:54 +0900

    remove quad on rclick

--- vynil_id/utils/annotation.py
+++ vynil_id/utils/annotation.py
@@ -18,6 +18,7 @@ class Annotater(object):
     def __init__(self, image_path):
         self.original_image = cv2.imread(image_path)
         self.clone = self.original_image.copy()
+        self.image_path = image_path
 
         window = cv2.namedWindow('image')
         cv2.moveWindow('image', 40,30)
@@ -35,27 +36,43 @@ class Annotater(object):
 
 
         elif event == cv2.EVENT_MBUTTONDOWN:
+            self.reshape_to_quad()
             self.draw_quad()
             self.image_coordinates.append([])
 
         # Clear last drawn quad on right mouse button click
         elif event == cv2.EVENT_RBUTTONDOWN:
             self.clone = self.original_image.copy()
+            if len(self.image_coordinates)>1:
+                if len(self.image_coordinates[-1])==0:
+                    self.image_coordinates.pop()
+                self.image_coordinates.pop()
+                self.draw_quads()
+            else:
+                self.image_coordinates = [[]]
+
             cv2.imshow("image",self.clone)
 
-        clear
 
-    def draw_quad(self, internal = True):
-        self.image_coordinates[-1] = np.stack(self.image_coordinates[-1], axis=0).reshape((-1,1,2)).astype(np.int32)
-        if len(self.image_coordinates[-1]) > 4:
-            self.image_coordinates[-1] = self.reduce_polygon_to_q()
+
+    def reshape_to_quad(self, idx = -1):
+        self.image_coordinates[idx] = np.stack(self.image_coordinates[idx], axis=0).reshape((-1,1,2)).astype(np.int32)
+        if len(self.image_coordinates[idx]) > 4:
+            self.image_coordinates[idx] = self.reduce_polygon_to_q()
+        pass
+
+    def draw_quad(self, internal = True, idx = -1):
         ##draw hull on image and show
 
-        cv2.drawContours(self.clone, [self.image_coordinates[-1]], 0, (0,255,0), 3)
+        cv2.drawContours(self.clone, [self.image_coordinates[idx]], 0, (0,255,0), 3)
         cv2.imshow("image", self.clone)
         if internal == False:
             cv2.waitKey(2000)
         pass
+
+    def draw_quads(self):
+        for i in range(len(self.image_coordinates)):
+            self.draw_quad(idx = i)
 
     def reduce_polygon_to_q(self):
         return cv2_tools.contour_to_quad(self.image_coordinates[-1])
@@ -63,11 +80,14 @@ class Annotater(object):
     def get_coords(self):
         return self.image_coordinates
 
-    def show_image(self):
+    def get_contour(self):
+        return
+
+    def get_image(self):
         return self.clone
 
-def save_json(location: str, annotations: dict):
-    json.dump(annotations, location, sort_keys=True, indent=4)
+    def save_json(self, location: str):
+        json.dump(self.annotations, location, sort_keys=True, indent=4)
 
 if __name__ == '__main__':
 
@@ -79,7 +99,7 @@ if __name__ == '__main__':
         annotater_widget = Annotater(f)
         key = 113
         while True:
-            cv2.imshow('image', annotater_widget.show_image())
+            cv2.imshow('image', annotater_widget.get_image())
             key_ = cv2.waitKey(0)
         # Close program with keyboard 'q'
             print(f'{key}, {key_}')
